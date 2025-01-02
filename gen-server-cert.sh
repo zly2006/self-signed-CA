bf2c30a36334fcf7c084400ed5c612850c378f65
Liyan Zhao <return65535@qq.com>
2025-01-02 20:35:58 +0800

    Add files via upload

---
+++ gen-server-cert.sh
@@ -0,0 +1,1 @@
+openssl req -new -key server.key -out server.csr -config csr.cnf && openssl req -new -key server.key -out server.csr -config csr.cnf && openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extfile csr.cnf -extensions v3_req
