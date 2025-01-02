22b51d215ccf6302d9be9a2fd81deedc021ab09c
Liyan Zhao <return65535@qq.com>
2025-01-02 20:37:31 +0800

    Update gen-server-cert.sh

--- gen-server-cert.sh
+++ gen-server-cert.sh
@@ -1,1 +1,3 @@
-openssl req -new -key server.key -out server.csr -config csr.cnf && openssl req -new -key server.key -out server.csr -config csr.cnf && openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extfile csr.cnf -extensions v3_req
+openssl genrsa -out server.key 4096
+openssl req -new -key server.key -out server.csr -config csr.cnf && \
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extfile csr.cnf -extensions v3_req
